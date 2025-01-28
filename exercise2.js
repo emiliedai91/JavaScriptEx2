fe874d1c13f841a2c48bf824b30a3ff6f2dbcb04
emiliedai91 <emilierdaigneault@gmail.com>
2025-01-28 18:46:41 -0500

    Add files via upload

---
+++ exercise2.js
@@ -0,0 +1,27 @@
+let userHeight = 170; 
+
+const userAge = 30;
+
+petNames = ["cinammon", "ginger", "atom"]; 
+petNames.push("apple");
+petNames.pop();
+petNames.reverse();
+
+console.log(petNames);
+
+car = {
+  make: "Toyota",
+  model: "Camry",
+  year: 1991,
+ 
+};
+
+console.log(car);
+
+car.year=2001;
+
+console.log(car);
+
+car.milage = 50400;
+
+console.log(car);
